f4e7d1a1e28e6838c4e2b6c340469b2274f40e39
hom9ki <156588482+hom9ki97@users.noreply.github.com>
2026-01-08 16:18:40 +0300

    edit loading and processing of price lists

--- backend/core/excel/price_list_edit.py
+++ backend/core/excel/price_list_edit.py
@@ -1,51 +1,65 @@
 import io
 
-from openpyxl import load_workbook, Workbook
 import pandas
 from pathlib import Path
+from settings import PRICE_SETTINGS
 
-from openpyxl.utils.dataframe import dataframe_to_rows
 
-PRICE_SETTINGS = {
-    'Спутник': {'Артикул': 1, 'Бренд': 2, 'Цена': 4, 'Кол-во': 5},
-    'МоскворечьеВИП': {'Производитель': 1, 'Номер производителя': 2,
-                       'Наличие на складе': 6, 'Цена, Рубль': 5},
-    'ТИСС МСК': {'Бренд': 1, 'Катал. номер': 3, 'ОПТ': 4, 'Кол-во всего': 5},
-    'BERG НСК': {'Артикул': 1, 'Бренд': 3, 'Количество': 5, 'Цена руб': 6},
-    'FORUM_AUTO_PRICE': {'ГРУППА': 1, '№ ПРОИЗВ.': 2, 'ЦЕНА, РУБ': 5, 'НАЛичие': 6},
-    'PriceTiss': {'Бренд': 1, 'Катал. номер': 3, 'ОПТ': 8, 'Кол-во всего': 10},
-    'Rossko': {'Бренд': 2, 'Артикул': 3, 'Цена, руб.': 7, 'Наличие': 9},
-    'АвтоРусь': {'Артикул': 2, 'Изготовитель': 3, 'Наличие': 4, 'ЦенаКлиенту': 6},
-    'БергВИП': {'Артикул': 1, 'Бренд': 3, 'Количество': 5, 'Цена руб': 6},
-    'Иверс': {'Произв.': 1, 'Номер запчасти': 6, 'Ост.': 7, 'Цена': 8},
-    'МХ групп': {'articul': 1, 'brand': 2, 'stock': 4, 'price': 5},
-    'ПрофитЛига': {'Номенклатура.Производитель': 1, 'Артикул': 2, 'Цена': 6, 'ПредставлениеОстатка': 7},
-    'РосскоВИП': {'Бренд': 2, 'Артикул': 3, 'Цена, руб.': 7},
-    'Шате-М': {'Бренд': 1, 'Каталожный номер': 2, 'Остаток': 4, 'Цена': 7},
-    'Шате-Подольск ВИП': {'Бренд': 1, 'Каталожный номер': 2, 'Остаток': 4, 'Цена': 7}
+def read_xlsx(file_bytes, file_name, extension):
+    try:
+        engine = 'openpyxl' if extension == '.xlsx' else 'xlrd'
+        df = pandas.read_excel(io.BytesIO(file_bytes), engine=engine)
+        return df
+    except Exception as e:
+        print(f"Ошибка при чтении файла {file_name}: {e}")
+        raise
 
-}
+
+def read_csv(file_bytes, file_name, encodings):
+    for encoding in encodings:
+        try:
+            df = pandas.read_csv(io.BytesIO(file_bytes), encoding=encoding, delimiter=';', low_memory=False)
+            return df
+        except UnicodeDecodeError:
+            continue
+        except Exception as e:
+            print(f"Ошибка при чтении файла {file_name} с кодировкой {encoding}: {e}")
+            continue
+    raise ValueError(f'Не удалось прочитать файл {file_name}')
+
+
+def read_file_data(file_bytes, file_name):
+    print(f'Чтение файла: {file_name}')
+    extension = Path(file_name).suffix
+    encodings = ['utf-8', 'cp1251', 'windows-1251', 'iso-8859-1', 'latin1']
+
+    if extension in ['.xls', '.xlsx']:
+        return read_xlsx(file_bytes, file_name, extension)
+
+    elif extension == '.csv':
+        return read_csv(file_bytes, file_name, encodings)
+
+    else:
+        raise ValueError(f'Неизвестный формат файла {file_name}')
 
 
 def read_excel(file_bytes, file_name):
-    print('Чтение файла')
+    print(f'Чтение файла: {file_name}')
     base_name = Path(file_name).stem
-    extension = Path(file_name).suffix
     if base_name not in PRICE_SETTINGS:
         print(f'Неизвестный файл {file_name}')
         return None
 
     columns = PRICE_SETTINGS[base_name]
     required_columns = list(columns.keys())
-    print('Чтение файла завершено')
-    print('Обработка файла')
+    print(f'Чтение файла {file_name} завершено')
 
-    df = pandas.read_excel(io.BytesIO(file_bytes), engine='openpyxl')
+    df = read_file_data(file_bytes, file_name)
 
     missing_columns = [col for col in required_columns if col not in df.columns]
     if missing_columns:
         raise ValueError(f"Отсутствуют необходимые столбцы: {missing_columns}")
-    print('Обработка файла')
+
     max_col = max(columns.values())
     data = {}
     for i in range(max_col):
@@ -66,7 +80,7 @@ def read_excel(file_bytes, file_name):
     with pandas.ExcelWriter(output_stream, engine='openpyxl') as writer:
         new_df.to_excel(writer, index=False, sheet_name='Лист1')
     output_stream.seek(0)
-    print('Обработка завершена')
+    print(f'Обработка файла {file_name} завершена')
     return output_stream.read()
 
 
