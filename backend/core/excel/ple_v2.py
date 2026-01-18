eec48667269ae1574b3802cec3b4c48336100393
xomma <hom9ki83@mail.ru>
2026-01-18 22:30:22 +0300

    create tests for class SmartColumnDetector and edit analyze title file

--- backend/core/excel/ple_v2.py
+++ backend/core/excel/ple_v2.py
@@ -12,16 +12,27 @@ class ColumnType(Enum):
     NAME = 'name'
     PRICE = 'price'
     QUANTITY = 'quantity'
+    UNDEFINED = 'undefined'
 
+
+# @dataclass
+# class ColumnInfo:
+#     index: int
+#     name: Optional[str]
+#     data_type = ColumnType
+#     confidence = float
+#     sample_value: List[Any]
+#     characteristics: Dict[str, Any]
 
 @dataclass
 class ColumnInfo:
     index: int
-    name: Optional[str]
-    data_type = ColumnType
-    confidence = float
-    sample_value: List[Any]
-    characteristics: Dict[str, Any]
+    data_type: ColumnType
+    confidence: float
+
+    @classmethod
+    def empty(cls):
+        return cls(index=0, confidence=0.0, data_type=ColumnType.UNDEFINED)
 
 
 class SmartColumnDetector:
@@ -45,6 +56,10 @@ class SmartColumnDetector:
                 r'^[<>≥≤]\s*\d+$',
                 r'^[<>≥≤]\d+$',
                 r'^[<>≥≤]\s*\d+[\.,]\d+$'
+            ],
+        ColumnType.BRAND:
+            [
+                r'^[A-Z0-9\s&.-]+$'
             ]
     }
 
@@ -58,27 +73,149 @@ class SmartColumnDetector:
 
             ],
     }
+
     def __init__(self):
         self.column_infos: List[ColumnInfo] = []
+        self.column_types = {}
 
-        def analyze_df(df: pd.DataFrame):
+    def analyze_df(self, df: pd.DataFrame):
 
-            for i, column in enumerate(df.columns):
-                column_data = df.iloc[:, i]
-                col_info = analyze_column(column_data, i, column)
+        if df.empty:
+            print('df is empty')
+            return None
+        stat = {}
+        for i, column in enumerate(df.columns):
+            column_data = df.iloc[:, i]
+            stat[i] = self.analyze_column(column_data, i, column, stat)
+        print(f'col_info: {stat}')
 
-        def analyze_column(column_data: pd.Series, index: int, name: str) -> ColumnInfo:
+        return None
 
-            series = get_simple_series(column_data)
+    def analyze_column(self, column_data: pd.Series, index: int, name: str, stat: dict):
 
+        series = self.get_simple_series(column_data)
+        res = self.simple_detect_column_type(series, index)
+        if res['contain_str']:
+            stat = self.detect_column_str_brand(series, index)
+        elif res['contain_int']:
+            stat = self.detect_column_int_brand(series, index)
+        elif res['contain_float']:
+            stat = self.detect_column_float_brand(series, index)
+        else:
+            raise ValueError('column type not found')
+        data = self.select_title(stat)
+        return data
 
-        def get_simple_series(series: pd.Series) -> List[Any]:
+    def get_simple_series(self, series: pd.Series) -> List[Any]:
 
-            no_nan_series = series.dropna()
-            if len(no_nan_series) == 0:
-                return []
-            simple_series = no_nan_series.sample(50, random_state=42).tolist()
+        no_nan_series = series.dropna()
+        if len(no_nan_series) == 0:
+            return []
+        simple_series = no_nan_series.sample(300, random_state=42).tolist()
 
-            return simple_series
+        return simple_series
 
+    def simple_detect_column_type(self, series: List[Any], index):
+        stat_series = {
+            'series_len': len(series),
+            'contain_int': False,
+            'contain_float': False,
+            'contain_str': False,
+            'percent_int': 0,
+            'percent_float': 0,
+            'percent_str': 0,
+        }
+        stat_values = {
+            'int_count': 0,
+            'float_count': 0,
+            'str_count': 0,
+        }
 
+        for value in series:
+            if isinstance(value, (int, np.integer)):
+                stat_values['int_count'] += 1
+            if isinstance(value, (float, np.floating)):
+                stat_values['float_count'] += 1
+            if isinstance(value, str):
+                if value.isdigit():
+                    stat_values['int_count'] += 1
+                else:
+                    stat_values['str_count'] += 1
+
+        stat_series['contain_int'] = stat_values['int_count'] / stat_series['series_len'] > 0.5
+        stat_series['contain_float'] = stat_values['float_count'] / stat_series['series_len'] > 0.5
+        stat_series['contain_str'] = stat_values['str_count'] / stat_series['series_len'] > 0.7
+        stat_series['percent_int'] = stat_values['int_count'] / stat_series['series_len']
+        stat_series['percent_float'] = stat_values['float_count'] / stat_series['series_len']
+        stat_series['percent_str'] = stat_values['str_count'] / stat_series['series_len']
+        return stat_series
+
+    def detect_column_str_brand(self, series: List[Any], index: int):
+        result = []
+        article_res = self.is_article(series, index)
+        result.append(article_res)
+        brand_res = self.is_brand(series, index)
+        result.append(brand_res)
+        return result
+
+    def is_article(self, series: List[Any], index: int) -> ColumnInfo:
+        confidence = 0
+        for value in series:
+            for pattern in self.PATTERNS[ColumnType.ARTICLE]:
+                if re.match(pattern, str(value)):
+                    confidence += 1
+                    break
+        return ColumnInfo(index=index, data_type=ColumnType.ARTICLE, confidence=confidence / len(series))
+
+    def is_brand(self, series: List[Any], index: int) -> ColumnInfo:
+        confidence = 0
+        for value in series:
+            for pattern in self.PATTERNS[ColumnType.BRAND]:
+                if re.match(pattern, str(value)):
+                    confidence += 1
+                    break
+        return ColumnInfo(index=index, data_type=ColumnType.BRAND, confidence=confidence / len(series))
+
+    def detect_column_int_brand(self, series: List[Any], index: int) -> List[ColumnInfo]:
+        result = []
+        quantity_res = self.is_quantity(series, index)
+        result.append(quantity_res)
+        return result
+
+    def is_quantity(self, series: List[Any], index: int) -> ColumnInfo:
+        confidence = 0
+        for value in series:
+            for pattern in self.PATTERNS[ColumnType.QUANTITY]:
+                if re.match(pattern, str(value)):
+                    confidence += 1
+                    break
+        return ColumnInfo(index=index, data_type=ColumnType.QUANTITY, confidence=confidence / len(series))
+
+    def detect_column_float_brand(self, series: List[Any], index: int):
+        result = []
+        price_res = self.is_price(series, index)
+        result.append(price_res)
+        return result
+
+    def is_price(self, series: List[Any], index: int) -> ColumnInfo:
+        confidence = 0
+        for value in series:
+            for pattern in self.PATTERNS[ColumnType.PRICE]:
+                if re.match(pattern, str(value)):
+                    confidence += 1
+                    break
+        return ColumnInfo(index=index, data_type=ColumnType.PRICE, confidence=confidence / len(series))
+
+    def select_title(self, stat: List[ColumnInfo]) -> ColumnInfo:
+        if len(stat) <= 0:
+            return ColumnInfo.empty()
+        else:
+            index = 0
+            confidence = 0
+            for i, item in enumerate(stat):
+                if item.confidence > confidence:
+                    confidence = item.confidence
+                    index = i
+            if confidence == 0:
+                return ColumnInfo.empty()
+            return stat[index]
--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .excel.ple_v2 import SmartColumnDetector
+import pandas as pd
 
-# Create your tests here.
+
+class TestSmartColumnDetector(TestCase):
+
+    def test_create_data(self):
+        data = [
+            ['f-a11103c3', 'a11103c3', 'FENOX', 'Амортизатор', 1276, 1],
+            ['xair-afco197', 'afco197', 'AIRLINE', 'Ароматизатор Кофе в мешочке со спреем горячий шоколад (AFCO197)',
+             204.36, 4],
+            ['xfbs-fdsb-cb4r', 'fdsb-cb4r', 'FEBEST', 'ВТУЛКА ЗАДНЕГО СТАБИЛИЗАТОРА D18 FORD FOCUS II CB4 2008-2011',
+             396, 1],
+            ['xjik-cd22001', 'cd22001', 'JIKIU', 'Пыльник шруса Infiniti  Nissan', 1229, 1],
+            ['xly-g32125r', 'g32125r', 'LYNXauto', 'Стойка амортизаторная передняя NISSAN Almera (N15) 1.4-2.0D 95-00',
+             3113, 1],
+            ['xm-h04-0958', 'h04-0958', 'METELLI', 'Цилиндр тормозной', 1323.61, 1],
+            ['xma-17539a', '17539a', 'MALO', 'Патрубок масляного радиатора', 196.81, 2]
+        ]
+
+        files = [
+            'D:/tests_data/МИКАДО.xlsx',
+            'D:/tests_data/АрмтекВИП.xlsx',
+        ]
+        for file in files:
+            df = pd.read_excel(file)
+
+            processor = SmartColumnDetector()
+            processor.analyze_df(df)
